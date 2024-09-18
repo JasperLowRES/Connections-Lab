a2531935580eb0c53d2242041bb40cd3fde3ff5b
JasperLowRES <jgh9146@nyu.edu>
2024-09-18 07:41:36 -0700

    Add files via upload

---
+++ week2/script.js
@@ -0,0 +1,7 @@
+document.getElementById('day-button').addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('sun.gif')";
+});
+
+document.getElementById('night-button').addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('moon.gif')";
+});
